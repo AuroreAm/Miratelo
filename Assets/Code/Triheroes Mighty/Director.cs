68e9d1c7babbf52255a8cded38b1336f66da8777
SartinPixel <96411452+SartinPixel@users.noreply.github.com>
2025-06-17 08:56:18 +0300

    added Director

---
+++ Assets/Code/Triheroes Mighty/Director.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Pixify;
+using UnityEngine;
+
+namespace Triheroes.Code
+{
+    /// <summary>
+    /// director of the triheroes Mighty
+    /// </summary>
+    public class Director : Character
+    {
+        
+        public static Director o;
+        public Director ()
+        {
+            o = this;
+        }
+
+    }
+}
